75b8058525e9eabba0c0f96126dcfd671103acd7
ManjiCoder <manjicoder@gmail.com>
2023-03-25 01:39:53 +0530

    Logic to generate password React Native - ✔ video 22 completed - ✔

--- App.tsx
+++ App.tsx
@@ -26,9 +26,17 @@ const App = () => {
   const generatedPasswordString = (passwordLength: number) => {
     //
   };
-  const createPasword = (characters: string, passwordLength: number) => {
-    //
+
+  // Bussiness Logic
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = '';
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
   };
+
   const resetPasswordState = () => {
     //
   };
